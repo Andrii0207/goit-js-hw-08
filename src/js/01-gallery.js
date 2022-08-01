d7b49620475e35bb2fef83773486cbe807a7a0f7
Andrii Popov <popov.andrey80@gmail.com>
2022-08-01 23:19:15 +0300

    finish task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+function onCreateListImg(galleryItems) {
+  return galleryItems.reduce(
+    (acc, item) =>
+      acc +
+      `<a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" 
+              alt="${item.description}}" />
+      </a>`,
+    '',
+  );
+}
+
+const onCreateGallery = onCreateListImg(galleryItems);
+
+galleryEl.innerHTML = onCreateGallery;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+console.log(lightbox);
