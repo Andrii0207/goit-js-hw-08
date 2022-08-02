f16c02f844e5ab923359c20499a14707cf436239
Andrii Popov <popov.andrey80@gmail.com>
2022-08-02 23:15:26 +0300

    task-02 (not finish)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
