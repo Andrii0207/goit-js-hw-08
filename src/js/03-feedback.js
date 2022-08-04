54fbab66adbb283a9ebe37ce5139ca9acc85fff1
Andrii Popov <popov.andrey80@gmail.com>
2022-08-04 23:01:46 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,75 @@
+// inport '..'
 
-const inputRef = document.querySelector()
+// const form = document.querySelector('.feedback-form');
+// console.log(form);
+// const inputEmail = document.querySelector('input');
+// console.log(inputEmail);
+// const inputMessageForm = document.querySelector('textarea');
+// console.log(inputMessageForm);
+// const submitButton = document.querySelector('button');
+// console.log(submitButton);
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('input'),
+  inputMessage: document.querySelector('textarea'),
+  submitButton: document.querySelector('button'),
+};
+// console.log(refs);
+
+// const LOCALSTORAGE_KEYS = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.inputEmail.addEventListener('input', onEmailInput);
+refs.inputMessage.addEventListener('input', onInputMessage);
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+// onGetDataLocalStorage();
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formData = new FormData(refs.form);
+//   //   console.log(formData);
+//   formData.forEach((value, name) => console.log(value, name));
+// }
+
+// refs.form.addEventListener('change', evt => {
+//   LOCALSTORAGE_KEYS[event.target.name] = event.target.value;
+//   console.log(LOCALSTORAGE_KEYS);
+//   localStorage.setItem('LOCALSTORAGE_KEYS', JSON.stringify(LOCALSTORAGE_KEYS));
+// });
+
+// function initForm() {
+//   let getDataLocalStorage = localStorage.getItem(LOCALSTORAGE_KEYS);
+//   if (getDataLocalStorage) {
+//     getDataLocalStorage = JSON.parse(getDataLocalStorage);
+//     console.log(getDataLocalStorage);
+//   }
+// }
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+
+  console.log('Отправляем форму');
+}
+
+function onEmailInput(e) {
+  const inputEmail = e.currentTarget.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, inputEmail);
+  //   console.log(inputEmail);
+}
+
+function onInputMessage(e) {
+  const inputMessage = e.currentTarget.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, inputMessage);
+  console.log(inputMessage);
+}
+
+function getDataLocalStorage() {
+  const savedMessage = localStorage.getItem('LOCALSTORAGE_KEY');
+  console.log(savedMessage);
+  //   if (savedMessage) {
+  //     console.log(savedMessage);
+  //   }
+}
