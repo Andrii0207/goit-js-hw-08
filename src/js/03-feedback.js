6240c711fc377cdca790fcdaabf82d7022f32140
Andrii Popov <popov.andrey80@gmail.com>
2022-08-11 21:07:20 +0300

    fix mentor comment

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,23 +8,17 @@ formRef.addEventListener('input', throttle(onFormDataInput, 500));
 formRef.addEventListener('submit', onFormSubmit);
 
 let LOCALSTORAGE_KEY = 'feedback-form-state';
-
-const formData = {};
+let formData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
 
 onInitForm();
 
 function onFormDataInput(event) {
-  const message = event.target.value;
-  const key = event.target.name;
-
-  formData[key] = message;
-
+  formData[event.target.name] = event.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
-
   event.target.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 }
@@ -42,36 +36,3 @@ function onInitForm() {
     messageRef.value = savedMessage.message || '';
   }
 }
-
-// import throttle from 'lodash.throttle';
-
-// const formEl = document.querySelector('.feedback-form');
-// formEl.addEventListener('input', throttle(onFormData, 500));
-// formEl.addEventListener('submit', onSubmitForm);
-// initForm();
-// function onFormData(e) {
-//   let existedData = localStorage.getItem('feedback-form-state');
-//   if (existedData) {
-//     existedData = JSON.parse(existedData);
-//   } else {
-//     existedData = {};
-//   }
-//   existedData[e.target.name] = e.target.value;
-//   localStorage.setItem('feedback-form-state', JSON.stringify(existedData));
-// }
-// function onSubmitForm(e) {
-//   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-//   e.preventDefault();
-//   e.currentTarget.reset();
-//   localStorage.removeItem('feedback-form-state');
-// }
-// function initForm() {
-//   let existedData = localStorage.getItem('feedback-form-state');
-//   if (existedData) {
-//     existedData = JSON.parse(existedData);
-//     console.dir(Object.entries);
-//     Object.entries(existedData).forEach(([name, value]) => {
-//       formEl.elements[name].value = value;
-//     });
-//   }
-// }
